2425887108889bffd02c1b41697d0d8c7ca460be
kk <>
2021-02-23 23:36:25 +0900

    validation,create,flash day12参照

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,19 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: '保存しました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
 end
