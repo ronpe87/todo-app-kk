f1e02177074d76a822872eaca1eae78002f64128
kk <>
2021-02-24 23:25:22 +0900

    記事一覧の表示

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,6 @@
 class BoardsController < ApplicationController
   def index
-    @board = Board.first
+    @boards = Board.all
   end
 
   def new
@@ -8,9 +8,9 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
-    if @article.save
-      redirect_to article_path(@article), notice: '保存しました'
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board), notice: '保存しました'
     else
       flash.now[:error] = '保存に失敗しました'
       render :new
