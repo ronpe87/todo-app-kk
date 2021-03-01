dabb4728cc4cac7a23c5338e6a33927e60082a19
kk <>
2021-03-01 22:55:02 +0900

    userと記事の紐付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_121308) do
+ActiveRecord::Schema.define(version: 2021_03_01_125250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_02_17_121308) do
     t.string "title"
     t.text "content"
     t.date "limit"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
