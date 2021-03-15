0412e0b8f3734fa1969c0bfded52beb5470571e2
kk <>
2021-03-16 00:23:16 +0900

    comment追加と一覧表示,scss修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,7 @@ class TasksController < ApplicationController
   def show
     @board = Board.find(params[:board_id])
     @task = Task.find(params[:id])
+    @comments = @task.comments
   end
 
   def edit
